a31b132682ba0fea36c76a3b5d3255c2880343d1
George K Thomas <mtgamer023@gmail.com>
2024-01-15 16:49:02 +0530

    Wrk

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/milengeorge/",
+  base: "/milengeorge.github.io/",
   plugins: [react()],
 
 })
