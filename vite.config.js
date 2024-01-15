5927f8814a4aa618b542a57427f750be28b6206a
George K Thomas <mtgamer023@gmail.com>
2024-01-15 16:33:13 +0530

    Base change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/milengeorge.github.io/",
+  base: "/milengeorge/",
   plugins: [react()],
 
 })
